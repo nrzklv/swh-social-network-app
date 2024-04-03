28edd4bb22e2c4df082677343105a58494791d3c
nsx_69 <mrmanafest16@gmail.com>
2024-04-03 13:56:42 +0500

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <header></header>
+            <nav></nav>
+            <main></main>
+            <footer></footer>
+        </div>
+    );
+}
+
+export default App;
